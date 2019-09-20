bec17a3d50b3eb343a7d365ad2634ebedb5ea5f1
Hongchao Qi <hcqi0086@gmail.com>
2019-09-20 08:30:10 +0000

    version 0.1.1

--- R/SampleSize4ClinicalTrials.R
+++ R/SampleSize4ClinicalTrials.R
@@ -1,13 +1,13 @@
-#' @title Sample Size Calculation for Mean and Proportion Comparisons in Phase 3 Clinical Trials
+#' @title Sample Size Calculation for the Comparison of Means or Proportions in Phase III Clinical Trials
 #'
-#' @description This package aims to calculate sample size for mean and proportion
-#' comparisons in phase 3 clinical trials.
+#' @description This function aims to calculate sample size for the comparison of means or proportions
+#' in Phase III clinical trials.
 #'
 #' @param cat Type of the outcome for comparison.
 #'  \cr "m"
-#'    \cr stands for means comparison.
+#'    \cr stands for the comparison of means.
 #'  \cr "p"
-#'    \cr stands for proportions comparison.
+#'    \cr stands for the comparison of proportions.
 #' @param design The design of the clinical trials.
 #'   \cr 1
 #'     \cr Testing for equality
@@ -17,66 +17,60 @@
 #'     \cr Non-inferiority trial
 #'   \cr 4
 #'     \cr Equivalence trial.
-#' @param ratio The ratio between number of subjects in experimental drug and that in standard drug.
-#' @param alpha Type 1 error rate
-#' @param power Statistical power of the test (1-type 2 error rate)
-#' @param sigma The variance of observed outcomes in both arms (specified in means comparison for continuous outcomes)
-#' @param p1 The response rate of the experimental drug (specified proportion comparison for binary outcomes)
-#' @param p2 The response rate of the standard drug
-#' @param theta The difference between means or proportions in two arms
+#' @param ratio The ratio between the number of subjects in the treatment arm and that in the control arm.
+#' @param alpha Type I error rate
+#' @param power Statistical power of the test (1-type II error rate)
+#' @param sigma The variance of observed outcomes in both arms (specified in the comparison of means for continuous outcomes)
+#' @param p1 The response rate of the treatment arm (specified the comparison of proportions for binary outcomes)
+#' @param p2 The response rate of the control arm
+#' @param theta The difference between means or proportions in the two arms
 #' @param delta The prespecified non-inferiority or equivalence margin in non-inferiority or equivalence trials
 #'
 #' @return samplesize
 #'
-#' @usage ssc(cat=c("m","p"),design=c(1,2,3,4),ratio,alpha,power,sigma,p1,p2,theta,delta)
+#' @usage ssc(cat=c("m","p"), design=c(1,2,3,4), ratio, alpha, power, sigma, p1, p2, theta, delta)
 #'
 #' @importFrom stats qnorm
 #'
 #' @examples
-#' ##Means comparison for continuous outcomes, a non-inferiority trial with a non-inferiority margin 0.5
+#' ##The comparison of means, a non-inferiority trial with a non-inferiority margin 0.5
 #' ##the true treatment difference is assumed to be zero in non-inferiority and equivalence trials
 #' ssc(cat="m",design=3,ratio=1,alpha=0.05,power=0.9,sigma=1,theta=0,delta=0.5)
-#' ##Proportions comparison for binary outcomes, a superiority trial
+#' ##The comparison of proportions, a superiority trial
 #' ssc(cat="p",design=2,ratio=3,alpha=0.025,power=0.8,p1=0.4,p2=0.2,theta=0.2)
 #' @references Yin, G. (2012). Clinical Trial Design: Bayesian and Frequentist Adaptive Methods. John Wiley & Sons.
 #'
 #' @export
 
-###sample size calculation
+##Sample size calculation
 ssc<-function(cat=c("m","p"),design=c(1,2,3,4),ratio,alpha,power,sigma=0,p1=0,p2=0,theta,delta){
-  ##assign values to zalpha
+  ##Assign values to z_alpha
   if (design==1)
-    zalpha<-qnorm(1-(alpha/2))
+    z_alpha<-qnorm(1-(alpha/2))
   else
-    zalpha<-qnorm(1-alpha)
-  ##assign values to zbeta
+    z_alpha<-qnorm(1-alpha)
+  ##Assign values to z_beta
   if (design<4)
-    zbeta<-qnorm(power)
+    z_beta<-qnorm(power)
   else
-    zbeta<-qnorm((1+power)/2)
-  ##denominator
+    z_beta<-qnorm((1+power)/2)
+  ##Denominator
   if (design<3)
     denom<-theta^2
   if (design==3)
     denom<-(theta+delta)^2
   if (design==4)
     denom<-(delta-abs(theta))^2
-  ##sample size equation, n2 means number of subjects in standard treatment group
-  n2<-ceiling((1+1/ratio)*(sigma^2)*(zalpha+zbeta)^2/denom)
+  ##Sample size equation, n2 means number of subjects in the control arm
+  n2<-ceiling((1+1/ratio)*(sigma^2)*(z_alpha+z_beta)^2/denom)
   n1<-ratio*n2
   pbar<-(p1+p2)/2
-  ##pooled variance, n4 means number of sujects in standard treatment group
-  n4<-ceiling(((1+1/ratio)*(zalpha*sqrt(pbar*(1-pbar))+zbeta*sqrt((p1*(1-p1)+p2*(1-p2)*ratio)/(ratio+1)))^2)/denom)
+  ##Pooled variance, n4 means number of sujects in the control arm
+  n4<-ceiling(((1+1/ratio)*(z_alpha*sqrt(pbar*(1-pbar))+z_beta*sqrt((p1*(1-p1)+p2*(1-p2)*ratio)/(ratio+1)))^2)/denom)
   n3<-ratio*n4
   if (cat=="m")
-    samplesize<-list(Category="Sample size for means comparison",Experimental=c("Experimental drug:",n1),Standard=c("Standard drug:",n2))
+    samplesize<-list(Category="Sample size for the comparison of means",Treatment=c("The treatment arm:",n1),Control=c("The control arm:",n2))
   else
-    samplesize<-list(Category="Sample size for proportions comparison",Experimental=c("Experimental drug:",n3),Standard=c("Standard drug:",n4))
+    samplesize<-list(Category="Sample size for the comparison of comparisons",Treatment=c("The treatment arm:",n3),Control=c("The control arm:",n4))
   return(samplesize)
 }
-
-
-
-
-
-
---
+++ R/manual_namespace_gen.R
@@ -0,0 +1,10 @@
+#' Sample Size Calculation for the Comparison of Means or Proportions in Phase III Clinical Trials
+#'
+#' There are four categories for Phase III clinical trials according to different research goals, including (1) Testing for equality, (2) Superiority trial, (3) Non-inferiority trial, and (4) Equivalence trial. This package aims to help researchers to calculate sample size when comparing means or proportions in Phase III clinical trials with different research goals. The ssc function can calculate the sample size with pre-specified type I error rate, statistical power and effect size.
+#'
+#' @docType package
+#'
+#' @author Hongchao Qi, Fang Zhu
+#'
+#' @name SampleSize4ClinicalTrials
+NULL
